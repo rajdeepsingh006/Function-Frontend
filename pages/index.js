b8d33f0f7b49a8ab7776b8c6591a412d33f5c1ea
rajdeepsingh006 <147084619+rajdeepsingh006@users.noreply.github.com>
2024-03-24 16:05:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,123 @@
+import {useState, useEffect} from "react";
+import {ethers} from "ethers";
+import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+
+export default function HomePage() {
+  const [ethWallet, setEthWallet] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const atmABI = atm_abi.abi;
+
+  const getWallet = async() => {
+    if (window.ethereum) {
+      setEthWallet(window.ethereum);
+    }
+
+    if (ethWallet) {
+      const account = await ethWallet.request({method: "eth_accounts"});
+      handleAccount(account);
+    }
+  }
+
+  const handleAccount = (account) => {
+    if (account) {
+      console.log ("Account connected: ", account);
+      setAccount(account);
+    }
+    else {
+      console.log("No account found");
+    }
+  }
+
+  const connectAccount = async() => {
+    if (!ethWallet) {
+      alert('MetaMask wallet is required to connect');
+      return;
+    }
+  
+    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
+    handleAccount(accounts);
+    
+    // once wallet is set we can get a reference to our deployed contract
+    getATMContract();
+  };
+
+  const getATMContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethWallet);
+    const signer = provider.getSigner();
+    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
+ 
+    setATM(atmContract);
+  }
+
+  const getBalance = async() => {
+    if (atm) {
+      setBalance((await atm.getBalance()).toNumber());
+    }
+  }
+
+  const deposit = async() => {
+    if (atm) {
+      let userInput = prompt('Enter the number of tokens to mint:');
+      console.log('User input:', userInput);
+      let tx = await atm.deposit(userInput);
+      await tx.wait()
+      getBalance();
+    }
+  }
+
+  const withdraw = async() => {
+    if (atm) {
+      let userInput = prompt('Enter the number of tokens to burn:');
+      console.log('User input:', userInput);
+      let tx = await atm.withdraw(userInput);
+      await tx.wait()
+      getBalance();
+    }
+  }
+
+  const initUser = () => {
+    // Check to see if user has Metamask
+    if (!ethWallet) {
+      return <p>Please install Metamask in order to use this ATM.</p>
+    }
+
+    // Check to see if user is connected. If not, connect to their account
+    if (!account) {
+      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+    }
+
+    if (balance == undefined) {
+      getBalance();
+    }
+
+    return (
+      <div>
+       <h3> <font color="#ff0000"> <p>Account Connected: {account}</p></font></h3>
+       <h3><font color="#ff0000">  <p>Account Balance: {balance}</p></font></h3>
+       <h3> <button onClick={deposit}><font color="blue"> Mint RAJ</font></button></h3>
+       <h3> <button onClick={withdraw}><font color="blue">Burn RAJ</font></button></h3>
+      </div>
+    )
+  }
+
+  useEffect(() => {getWallet();}, []);
+
+  return (
+    <main className="container">
+      <header><h1>RAJ EXCHANGE</h1></header>
+      {initUser()}
+      <style jsx>{`
+        .container {
+          text-align: center
+        }
+      
+      `}
+      </style>
+    </main>
+  )
+}
 
